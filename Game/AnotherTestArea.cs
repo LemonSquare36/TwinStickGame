ccd372b66424c192746c1ae3fc8ab2b3983ce667
jglover <jglover@PRG23656>
2017-02-28 09:21:53 -0500

    Set up AnotherTestArea

---
+++ Game/AnotherTestArea.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using System.Diagnostics;
+
+namespace TwinStick
+{
+    class AnotherTestArea : AreaManager
+    {
+
+        public override void Initialize()
+        {
+
+        }
+
+        public override void LoadContent(SpriteBatch spriteBatchmain)
+        {
+            spriteBatch = spriteBatchmain;
+
+        }
+
+        public override void Update(Camera camera, GraphicsDeviceManager graphicsManager)
+        {
+            getKey();
+        }
+
+        public override void Draw()
+        {
+
+        }
+    }
+}
